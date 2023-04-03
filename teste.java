662233fc7c718170db30492434d50c75fb151ad5
Vitor Vilas Boas <44900046+vitorvilasboas@users.noreply.github.com>
2023-04-03 06:37:23 -0300

    Add files via upload

---
+++ teste.java
@@ -0,0 +1,16 @@
+//package aula3110;
+import java.io.*;
+public class teste {
+
+	public static void main(String[] args) {
+		String texto;
+		texto = "QUEREMOS INTERVALO!!!";
+		System.out.println(texto);
+		//impressão sem quebra de linha
+		System.out.print("O vitor");
+		//impressão com quebra de linha
+		System.out.println("é viciado gajhdkjas");
+		System.out.println("em PES 2013");
+	}
+
+}
---
+++ vitor.java
@@ -0,0 +1,5 @@
+public class vitor {
+	public static void main(String[] args) {
+		System.out.println("Vitor teste!!!");
+	}
+}
